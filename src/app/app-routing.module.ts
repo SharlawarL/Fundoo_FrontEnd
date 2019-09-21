419794f20a87e596352b53267f2992274e59886c
SharlawarL <sharlawar77@gmail.com>
2019-09-21 12:47:16 +0530

    the routing rout will be into the file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,52 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReminderComponent } from './reminder/reminder.component';
+import { TrashComponent } from './trash/trash.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { HomeComponent } from './home/home.component';
+import { NotesComponent } from './notes/notes.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ResetComponent } from './reset/reset.component';
 
 const routes: Routes = [
-  { path :'login',component:LoginComponent },
-  { path : 'register ', component:RegisterComponent }
+  {
+    path : 'login',
+    component:LoginComponent
+  },
+  {
+    path : 'register',
+    component:RegisterComponent
+  },
+  {
+    path : '',
+    component:HomeComponent
+  },
+  {
+    path : 'notes',
+    component:NotesComponent,
+    //canActivate: [AuthGuard]
+  },
+  {
+    path : 'forgot',
+    component:ForgotComponent
+  }
+  ,
+  {
+    path : 'reset',
+    component:ResetComponent
+  },
+  {
+    path : 'dashboard',
+    component: DashboardComponent,
+    children:[
+      { path:'notes', component: NotesComponent },
+      { path:'reminder', component: ReminderComponent },
+      { path:'archive', component: ArchiveComponent },
+      { path:'trash', component: TrashComponent }
+    ]
+  }
 ];
 
 @NgModule({
@@ -13,3 +55,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const rountingComponent = [DashboardComponent,
+  NotesComponent,ReminderComponent,ArchiveComponent,TrashComponent]

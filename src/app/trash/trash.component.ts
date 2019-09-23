dbc5e94af02f285de67e26f7ab53f5cf760db397
SharlawarL <sharlawar77@gmail.com>
2019-09-23 18:05:34 +0530

    notes trash will be shown into the file

--- src/app/trash/trash.component.ts
+++ src/app/trash/trash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../service/notes.service';
 
 @Component({
   selector: 'app-trash',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrashComponent implements OnInit {
 
-  constructor() { }
+  public data: any;
+  public temp_data: any;
+  loading: boolean;
+  public Token = localStorage.getItem('User')
+  public Note_view ="";
+  view:String;
+
+  constructor(
+    private Notes: NotesService
+  ) { }
 
   ngOnInit() {
+    //loading data on load
+    this.get_Notes()
+    //this.Notes.currentView.subscribe(view => this.view = view)
+  }
+
+  // Get the Notes which are store into database
+  get_Notes(){
+    const user_token = this.Token
+    //getting data from service
+    this.Notes.Get_Notes(user_token).subscribe(Note_data=>{
+      this.data = Note_data
+      console.log(Note_data)
+    })
   }
 
 }
+

3134fcefabbbb9b0b47f15441c783fad910553fb
SharlawarL <sharlawar77@gmail.com>
2019-09-23 17:54:34 +0530

    notes will be archive and unarchive

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../service/notes.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-archive',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchiveComponent implements OnInit {
 
-  constructor() { }
+  public data: any;
+  public temp_data: any;
+  loading: boolean;
+  public token = localStorage.getItem('User')
+  public Note_view ="";
+  view:String;
+
+  constructor(
+    private Notes: NotesService,
+    private route: ActivatedRoute,
+    private rout: Router
+  ) { }
 
   ngOnInit() {
+    //loading data on load
+    this.get_Notes()
+    //this.Notes.currentView.subscribe(view => this.view = view)
   }
 
+  // Get the Notes which are store into database
+  get_Notes(){
+    const user_token = this.token
+    //getting data from service
+    this.Notes.Get_Notes(user_token).subscribe(Note_data=>{
+      this.data = Note_data
+      console.log(Note_data)
+    })
+  }
+
+  //for archive the Notes
+  archive(note_data : any)
+  {
+    let data = "note_id="+note_data.note_id+"&&token="+this.token
+    this.Notes.getarchive(data).subscribe()
+    this.get_Notes()
+    this.rout.navigate(['/dashboard/archive'])
+  }
+
+  
+
 }
+
+

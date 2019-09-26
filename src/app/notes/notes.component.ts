817342890f242c0bd0eb1a9fe0776cbff4254c80
SharlawarL <sharlawar77@gmail.com>
2019-09-26 18:08:12 +0530

    We can now attache and remove the lebels

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -26,29 +26,55 @@ export class NotesComponent implements OnInit{
   public model_id : any;
   public model_title : any;
   public model_Note : any;
+  public model_color : any;
   public model_box = false;
   public model_reminder : any;
   public showSetReminmder = true;
+  public showInputReminmder = true;
+  public x: number;
   success : any
   public note_view ="";
   public drag_note;
   public list:any;
   public color_set: any;
   view:String;
+  private reminder : "lalait";
 
   constructor(
-    private Notes: NotesService,
-    private route: ActivatedRoute,
-    private rout: Router) { 
-    }
+        private Notes: NotesService,
+        private route: ActivatedRoute,
+        private rout: Router) { 
+      }
 
   ngOnInit():void {
-    //loading data on load
-    this.get_Notes()
-    this.get_labels()
-    this.get_labelsnote()
-    this.get_color()
-    this.Notes.currentView.subscribe(view => this.view = view)
+        //loading data on load
+        this.get_Notes()
+        this.get_labels()
+        this.get_labelsnote()
+        this.get_color()
+        this.Notes.currentView.subscribe(view => this.view = view)
+  }
+
+  // Adding New Notes
+  AddNotes(event : any)
+  {
+    // data taking from user input
+    event.preventDefault()
+    const target = event.target
+    const title = target.querySelector('#title').value
+    const notes = target.querySelector('#notes').value
+    const reminder = target.querySelector('#reminder').value
+    const token = this.token
+    let notes_data = 'title='+title+'&Notes='+notes+'&user_id='+token+'&reminder='+reminder
+    this.show = true
+
+    // passing data towords to servece for inserting into database
+    this.Notes.createNotesPost(notes_data).subscribe(
+      data=>
+      {
+        this.ngOnInit()
+      }
+    )
   }
 
   Note_view_grid()
@@ -64,32 +90,34 @@ export class NotesComponent implements OnInit{
   showTakeNotes(){
     this.show = false;
   }
-// fo rcolor set
-get_color()
-{
-  const color  = [
-    {color:'#bfff00'},
-    {color:'#00ff00'},
-    {color:'#00ffff'},
-    {color:'#0080ff'},
-    {color:'#0099cc'},
-    {color:'#80ced6'},
-    {color:'#f0f0f0'},
-    {color:'#eca1a6'},
-    {color:'#6b5b95'},
-    {color:'#ffbf00'},
-    {color:'#ff8000'},
-    {color:'#8000ff'}
-  ]
-  this.color_set = color
-}
-change_color(color : any,id : any)
-{
-    let Notes_data = 'note_id='+id+'&&color='+color+'&&token='+this.token
-    this.Notes.Update_color(Notes_data).subscribe(data=>{ this.get_Notes()  })
-    this.get_Notes()
-    this.rout.navigate(['/dashboard/notes'])
-}
+  // fo rcolor set
+  get_color()
+  {
+    const color  = [
+      {color:'#bfff00'},
+      {color:'#00ff00'},
+      {color:'#00ffff'},
+      {color:'#0080ff'},
+      {color:'#0099cc'},
+      {color:'#80ced6'},
+      {color:'#f0f0f0'},
+      {color:'#eca1a6'},
+      {color:'#6b5b95'},
+      {color:'#ffbf00'},
+      {color:'#ff8000'},
+      {color:'#8000ff'}
+    ]
+    this.color_set = color
+  }
+
+  //chenge color function
+  change_color(color : any,id : any)
+  {
+      let Notes_data = 'note_id='+id+'&&color='+color+'&&token='+this.token
+      this.Notes.Update_color(Notes_data).subscribe(data=>{ 
+        this.ngOnInit()
+      })
+  }
 
   // Get the Notes which are store into database
   get_Notes(){
@@ -100,66 +128,66 @@ change_color(color : any,id : any)
     })
   }
 
-  //open model box
+  //open model box content
   modelBox(note){
     this.model_id = note.note_id
     this.model_title = note.title
     this.model_Note = note.Notes
+    this.model_color = note.color
     this.model_reminder = note.reminder
   }
+
   //show reminder set
   setRemider()
   {
     this.showSetReminmder = false;
-    console.log("reminder click")
   }
+
   //hide reminder set
   hideRemider()
   {
     this.showSetReminmder = true;
-    console.log("reminder unclick")
   }
-   //for adding reminder
-   Add_reminder(event : any)
-   {
+  //for adding reminder
+  Add_reminder(event : any)
+  {
      this.showSetReminmder = true;
      event.preventDefault()
      const target = event.target
      const reminder_set = target.querySelector('#rdate').value
      this.model_reminder = reminder_set
-   }
-   //update_reminder
-   update_reminder(event : any)
-   {
+  }
+  //update_reminder
+  update_reminder(event : any)
+  {
     event.preventDefault()
     const target = event.target
     const rdate = target.querySelector('#rdate').value
     const id = target.querySelector('#note_id').value
     let Notes_data = 'note_id='+id+'&&rdate='+rdate+'&&token='+this.token
-    this.Notes.Update_reminderdate(Notes_data).subscribe(data=>{ this.get_Notes()  })
-    this.get_Notes()
-    this.rout.navigate(['/dashboard/notes'])
-   }
-
-   //update notes
-   UpdateNotes(event: any)
-   {
-     event.preventDefault()
-     const target = event.target
-     const title = target.querySelector('#title').value
-     const Notes = target.querySelector('#Notes').value
-     const reminder = target.querySelector('#reminder').value
-     const id = target.querySelector('#Notes_id').value
-     let Notes_data = 'note_id='+id+'&&title='+title+'&&Notes='+Notes+'&&reminder='+reminder
-     this.Notes.Update_Notes(Notes_data).subscribe(data=>{
-        this.get_Notes()
-        this.rout.navigate(['/dashboard/notes'])
+    this.Notes.Update_reminderdate(Notes_data).subscribe(data=>{ 
+      this.ngOnInit()
     })
-   }
+  }
+
+  //update notes
+  UpdateNotes(event: any)
+  {
+    event.preventDefault()
+    const target = event.target
+    const title = target.querySelector('#title').value
+    const Notes = target.querySelector('#Notes').value
+    const reminder = target.querySelector('#reminder').value
+    const id = target.querySelector('#Notes_id').value
+    let Notes_data = 'note_id='+id+'&&title='+title+'&&Notes='+Notes+'&&reminder='+reminder
+    this.Notes.Update_Notes(Notes_data).subscribe(data=>{
+      this.ngOnInit()
+    })
+  }
 
 
-   //for drag nd drop of notes
-   drop(event: CdkDragDrop<string[]>) {
+  //for drag nd drop of notes
+  drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
       let drag_data = "previous="+event.previousIndex+"&&next="+event.currentIndex+"&&token="+this.token
@@ -179,22 +207,21 @@ change_color(color : any,id : any)
     this.Notes.addtrash(data).subscribe(
       data=>
       {
-        this.get_Notes()
-        this.rout.navigate(['/dashboard/notes'])
+        this.ngOnInit()
       }
     )
   }
+
   //for archive the Notes
   archive(note_data : any)
   {
     let data = "note_id="+note_data.note_id+"&&token="+this.token
     this.Notes.addarchive(data).subscribe(data=>
       {
-        this.get_Notes()
-        this.rout.navigate(['/dashboard/notes'])
-      })
-    
+        this.ngOnInit()
+      }) 
   }
+
   // Get the Notes which are store into database
   get_labels(){
     //getting data from service
@@ -204,37 +231,32 @@ change_color(color : any,id : any)
   })
   }
 
-  clicklabel(lebel : any,note_id: any)
+  //add lebel to notes
+  attach_label(id : number,note_id: any)
   {
-    let data = "lebel="+lebel+"&&note_id="+note_id+"&&token="+this.token
+    let data = "lebel_id="+id+"&&note_id="+note_id+"&&token="+this.token
     this.Notes.addlebelnotes(data).subscribe( data =>
       {
-        this.get_Notes()
-        this.get_labels()
-        this.get_labelsnote()
-        this.rout.navigate(['/dashboard/notes'])
+        this.ngOnInit()
       })
   }
 
   // Get the lebel notes which are store into database
   get_labelsnote(){
     //getting data from service
-    this.Notes.Get_labelsnote(this.token).subscribe(lebel_data=>{
-    this.lebel_data = lebel_data
-    this.get_Notes()
-    this.rout.navigate(['/dashboard/notes'])
-  })
+    this.Notes.Get_labelsnote(this.token).subscribe(
+      lebel_data=>{
+            this.lebel_data = lebel_data
+    })
   }
-  removelabel(lebel_id: any)
+  //remove lebel from notes
+  remove_label(lebel_id: number)
   {
     let data = "id="+lebel_id+"&&token="+this.token
     this.Notes.removelebel(data).subscribe(
       data =>
       {
-        this.get_Notes()
-        this.get_labels()
-        this.get_labelsnote()
-        this.rout.navigate(['/dashboard/notes'])
+        this.ngOnInit()
       }
     )
   }

156e7f4dee2c9896f24470fa1708320b37a154fc
SharlawarL <sharlawar77@gmail.com>
2019-09-27 19:24:17 +0530

    Facebook login will be done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 import { FormGroup } from '@angular/forms';
+import { AuthService } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { LoginService } from '../service/login.service'
+
 
 
 
@@ -18,13 +22,15 @@ export class LoginComponent implements OnInit {
   private box_color = "silver";
   public success : boolean;
   public message = "";
+  private user: SocialUser;
+  private loggedIn: boolean;
 
   public form = {
     resetToken : null
   }
 
   constructor (
-    private Auth:AuthService, private router : Router,
+    private Auth:AuthService, private router : Router, private login: LoginService,
     private route: ActivatedRoute) { 
       route.queryParams.subscribe(
         param =>{
@@ -33,31 +39,35 @@ export class LoginComponent implements OnInit {
       )
     }
   ngOnInit() {
-    if(this.success == null){
-      this.message = "";
-    }
-    if(this.success == true){
-        this.message ="Your email is verified... now you can login.."
-    }
-    if(!this.success == false){
-        this.message ="Anauthorised user..."
-    }
+
+    this.Auth.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(this.user)
+      this.login.Social_login(this.user).subscribe(
+          data=>{
+            
+          }
+      )
+    });
+    
   }
 
+  //login for user from site registered
   loginUser(event){
     event.preventDefault()
     const target = event.target
     const email = target.querySelector('#email').value
     const password = target.querySelector('#password').value
 
-    this.Auth.User_login(email,password).subscribe(data=>{
+    this.login.User_login(email,password).subscribe(data=>{
       const myObjStr = JSON.parse(data);
       if(myObjStr["success"])
       {
         localStorage.setItem('User',myObjStr["Token"])
         localStorage.setItem('Notes_View','Grid')
         this.router.navigate(['/dashboard/notes'])
-        this.Auth.setLog(true)
+        this.login.setLog(true)
       }else{
         console.log(myObjStr["message"])
         
@@ -73,9 +83,19 @@ export class LoginComponent implements OnInit {
         }else{
           this.Password = "";
         }
-        this.Auth.setLog(false)
+        this.login.setLog(false)
       }
       //console.log(data,"Data from to the server")
     })
+  }
+
+  // login for Google
+  signInWithGoogle(): void {
+    this.Auth.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+ 
+  // login for Facebook
+  signInWithFB(): void {
+    this.Auth.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 }
